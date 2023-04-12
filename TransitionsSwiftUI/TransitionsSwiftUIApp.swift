2676efbb702f366de578e52c480a3cc7e0a55cbd
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-12 22:04:30 +0500

    Initial Commit

---
+++ TransitionsSwiftUI/TransitionsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionsSwiftUIApp.swift
+//  TransitionsSwiftUI
+//
+//  Created by sss on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransitionsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
