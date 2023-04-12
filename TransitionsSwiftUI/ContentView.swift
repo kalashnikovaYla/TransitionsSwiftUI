9759945477067d7dda9c6004801613e44bcf0151
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-13 00:53:29 +0500

    Initial

--- TransitionsSwiftUI/ContentView.swift
+++ TransitionsSwiftUI/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isAlertShow = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack (alignment: .top){
+            
+            Color("background")
+            HStack {
+                Text("Transitions")
+                    .font(.title)
+                    .foregroundColor(.white)
+                Spacer()
+                questionMarkView
+            }
+            .blur(radius: isAlertShow ? 10: 0)
+            .padding()
+            
+            if isAlertShow {
+                alertView
+                   
+                    //.transition(AnyTransition.slide)
+                    //.transition(AnyTransition.move(edge: .bottom))
+                    //.transition(.offset(x: 200, y: -200))
+                    //.transition(.opacity)
+                    //.transition(.scale)
+                    //.transition(.slide.combined(with: .scale).combined(with: .opacity))
+                    //.transition(.move(edge: .trailing).combined(with: .scale(scale: 0.1, anchor: .topTrailing)))
+                    .transition(.asymmetric(insertion: .slide, removal: .move(edge: .bottom)))
+                    .zIndex(1)
+            }
         }
-        .padding()
+        
+    }
+    
+    var questionMarkView: some View {
+        Button {
+            withAnimation {
+                isAlertShow.toggle()
+            }
+        } label: {
+            Image(systemName: "questionmark.circle.fill")
+                .resizable()
+                .frame(width: 25, height: 25)
+                .foregroundColor(Color("action"))
+        }
+
+    }
+    
+    var alertView: some View {
+        VStack(spacing: 20) {
+            Text("The documents copied")
+                .foregroundColor(.white)
+            Image(systemName: "questionmark.circle.fill")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                
+            Button("Cancel") {
+                withAnimation {
+                    isAlertShow.toggle()
+                }
+            }.foregroundColor(Color("action"))
+        }
+        .padding(35)
+        .background(RoundedRectangle(cornerRadius: 20).fill(Color("alert")))
+        .padding(.top, 100)
+        .shadow(radius: 15)
     }
 }
 
